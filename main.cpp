72a78aebc7d22cb526dd1c76ab1c6a44faeac670
hyacinth237 <acast494@ucr.edu>
2024-11-16 09:32:15 -0800

    tasks

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int main() {
-    
+    //input user astrology sign
+    //function that returns a positive message
+    //vector of messages, return the value at a vector index
     return 0;
 }

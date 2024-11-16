2b245f89b92c6fabb7fb23599d2e670010c1b814
hyacinth237 <acast494@ucr.edu>
2024-11-16 10:54:19 -0800

    new features

--- main.cpp
+++ main.cpp
@@ -9,5 +9,9 @@ int main() {
     astrologySign(userNum);
     //function that returns a positive message
     //vector of messages, return the value at a vector index
+    //simplistic website for kids who want to learn about the universe
+    //stars of each sign
+    //little quiz like 16 personality but zodiac
+
     return 0;
 }
